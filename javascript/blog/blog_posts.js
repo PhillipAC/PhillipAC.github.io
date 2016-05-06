2b2132b87993b1d03b3f6bc381a70a3042627c00
phillipac <pclontz@madison.k12.al.us>
2016-05-06 16:48:53 +0000

    New Post

--- javascript/blog/blog_posts.js
+++ javascript/blog/blog_posts.js
@@ -16,3 +16,8 @@ mainBlog.newPost(
     "Play it <a href='/applets/trivia_game.html'>here!</a>",
     "5/4/2016"
 );
+mainBlog.newPost(
+    "Single Pages",
+    "Today I added some code to let each post be able to be displayed in a page by itself using URL parameters. Each post is displaid in the post.html website and a parameter of id=# will display the post with that id. Go ahead and try it out by click the title of this post, then try clicking on another post's title.",
+    "5/6/2016"
+);
