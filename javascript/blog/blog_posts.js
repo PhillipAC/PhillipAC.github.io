aa49a3f16d12ef82d8c504d28e1dd325058c0073
phillipac <pclontz@madison.k12.al.us>
2016-09-24 17:58:14 +0000

    new post

--- javascript/blog/blog_posts.js
+++ javascript/blog/blog_posts.js
@@ -21,3 +21,8 @@ mainBlog.newPost(
     "Today I added some code to let each post be able to be displayed in a page by itself using URL parameters. Each post is displaid in the post.html website and a parameter of id=# will display the post with that id. Go ahead and try it out by click the title of this post, then try clicking on another post's title.",
     "5/6/2016"
 );
+mainBlog.newPost(
+    "Ruby on Rails Web Apps",
+    "I absolutly building applications that other people can use. Over the past year I have learned to program web apps using the Ruby on Rails framework. Today I have added a new section to my portfolio to show off a few projects I have made. Feel free to take a look at the code!",
+    "9/24/2016"
+);
